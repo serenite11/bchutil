7e51914f1bcb50ad9110e728ae68dccc340d54c1
btcsuite <btcsuite@btcsuite.localhost>
2017-11-05 19:21:22 +0900

    Adding lost btc -> bch replacements

--- tx.go
+++ tx.go
@@ -1,15 +1,15 @@
-// Copyright (c) 2013-2016 The btcsuite developers
+// Copyright (c) 2013-2016 The bchsuite developers
 // Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
 
-package btcutil
+package bchutil
 
 import (
 	"bytes"
 	"io"
 
-	"github.com/btcsuite/btcd/chaincfg/chainhash"
-	"github.com/btcsuite/btcd/wire"
+	"github.com/bchsuite/bchd/chaincfg/chainhash"
+	"github.com/bchsuite/bchd/wire"
 )
 
 // TxIndexUnknown is the value returned for a transaction index that is unknown.
