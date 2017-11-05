1db7414eadd2412604fa004b76d4fa612d5aaabb
btcsuite <btcsuite@btcsuite.localhost>
2017-11-05 21:47:55 +0900

    Fixing the Copyright headers

--- base58/genalphabet.go
+++ base58/genalphabet.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2015 The bchsuite developers
+// Copyright (c) 2015 The btcsuite developers
 // Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
 
@@ -15,7 +15,7 @@ import (
 )
 
 var (
-	start = []byte(`// Copyright (c) 2015 The bchsuite developers
+	start = []byte(`// Copyright (c) 2015 The btcsuite developers
 // Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
 
